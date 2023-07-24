80f4ea4b1e1b6a0d8ac4a2d3c3aab9ca3602dc82
poiioq <101961422+poiioq@users.noreply.github.com>
2023-07-24 01:27:07 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,393 @@
+package assignment4;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.*;
+import javax.swing.border.*;
+
+import assignment4.Validation.ValidationException;
+/*
+ * Student Name: Ricky Wong
+ * Student ID: N01581738
+ * Section: IGA
+ * Logic: The class contains the main method and the GUI components.
+ */
+
+/*
+ *  ITC5201 – Assignment 4
+ *  I declare that this assignment is my own work in accordance with Humber Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students/social media.
+ *  Name: _Ricky Lok Ting Wong__ Student ID: ____N01581738_____ Date: ____2023-07-19______
+ */
+
+public class App extends JFrame {
+
+	// Declare GUI components
+	private JTextField tfId;
+	private JTextField tfLastName;
+	private JTextField tfFirstName;
+	private JTextField tfMi;
+	private JTextField tfAddress;
+	private JTextField tfCity;
+	private JTextField tfState;
+	private JTextField tfTelephone;
+
+	private JLabel lblId;
+	private JLabel lblLastName;
+	private JLabel lblFirstName;
+	private JLabel lblMi;
+	private JLabel lblAddress;
+	private JLabel lblCity;
+	private JLabel lblState;
+	private JLabel lblTelephone;
+
+	private JButton btnView;
+	private JButton btnInsert;
+	private JButton btnUpdate;
+	private JButton btnClear;
+
+	private JLabel lblPrompt;
+
+	private JPanel panelData;
+	private JPanel panelFirstRow;
+	private JPanel panelSecondRow;
+	private JPanel panelThirdRow;
+	private JPanel panelForthRow;
+	private JPanel panelFifthRow;
+
+	private JPanel panelBtn;
+
+	private DBUtils db;
+
+	App() {
+		createComponents();
+		configureComponents();
+		setFrameConfig();
+		getConnection();
+		configureActionHandlers();
+	}
+
+	public void getConnection() {
+		// Create a new connection
+		setPrompt("Database connecting ...");
+		try {
+			db = new DBUtils();
+			setPrompt("Database connected :)");
+		} catch (ClassNotFoundException e) {
+			setPrompt("Error: Unable to establish database connection - Driver not found.", true);
+			e.printStackTrace();
+		} catch (SQLException e) {
+			setPrompt("Error: Unable to establish database connection - Please check credentials.", true);
+			e.printStackTrace();
+		}
+	}
+
+	private void createComponents() {
+
+		// Create labels and text fields
+		lblId = new JLabel("ID");
+		tfId = new JTextField(12);
+		lblLastName = new JLabel("Last Name");
+		tfLastName = new JTextField(10);
+		lblFirstName = new JLabel("First Name");
+		tfFirstName = new JTextField(10);
+		lblMi = new JLabel("mi");
+		tfMi = new JTextField(3);
+		lblAddress = new JLabel("Address");
+		tfAddress = new JTextField(15);
+		lblCity = new JLabel("City");
+		tfCity = new JTextField(15);
+		lblState = new JLabel("State");
+		tfState = new JTextField(3);
+		lblTelephone = new JLabel("Telephone");
+		tfTelephone = new JTextField(10);
+		lblPrompt = new JLabel("");
+
+		// Create buttons
+		btnView = new JButton("View");
+		btnInsert = new JButton("Insert");
+		btnUpdate = new JButton("Update");
+		btnClear = new JButton("Clear");
+
+		FlowLayout deLayout = new FlowLayout(FlowLayout.LEFT);
+		// Create panels
+		panelData = new JPanel(deLayout);
+		panelFirstRow = new JPanel(deLayout);
+		panelSecondRow = new JPanel(deLayout);
+		panelThirdRow = new JPanel(deLayout);
+		panelForthRow = new JPanel(deLayout);
+		panelFifthRow = new JPanel(deLayout);
+		panelBtn = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+	}
+
+	private void configureComponents() {
+		// Use grid bag layout
+		setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		panelData.setLayout(new GridLayout(5, 1));
+
+		// Use a helper method to add all components to their corresponding containers
+		addComponents(panelFirstRow, lblId, tfId);
+		addComponents(panelSecondRow, lblLastName, tfLastName, lblFirstName, tfFirstName, lblMi, tfMi);
+		addComponents(panelThirdRow, lblAddress, tfAddress);
+		addComponents(panelForthRow, lblCity, tfCity, lblState, tfState);
+		addComponents(panelFifthRow, lblTelephone, tfTelephone);
+		addComponents(panelData, panelFirstRow, panelSecondRow, panelThirdRow, panelForthRow, panelFifthRow);
+		panelData.setBorder(new TitledBorder(new EtchedBorder(), "Staff Information"));
+
+		gbc.fill = GridBagConstraints.BOTH;
+		// set panelData to take 80% of the container height
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1;
+		gbc.weighty = 0.8;
+		add(panelData, gbc);
+
+		// set panelBtn to take 20% of the container height
+		addComponents(panelBtn, btnView, btnInsert, btnUpdate, btnClear);
+		gbc.gridy = 1;
+		gbc.weighty = 0.2;
+		add(panelBtn, gbc);
+
+		// set lblPrompt to be displayed at the bottom left
+		gbc.anchor = GridBagConstraints.SOUTHWEST;
+		gbc.gridy = 2;
+		gbc.weighty = 0;
+		add(lblPrompt, gbc);
+
+		// Set font and colors
+		lblPrompt.setFont(new Font("Arial", Font.ITALIC | Font.BOLD, 12));
+		tfId.setBackground(Color.YELLOW);
+
+		pack();
+	}
+
+	private void setFrameConfig() {
+		setTitle("");
+		setSize(500, 250);
+		setResizable(true);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private void addComponents(JPanel container, JComponent... args) {
+		for (JComponent comp : args) {
+			container.add(comp);
+		}
+	}
+
+	private void configureActionHandlers() {
+		// Collect all input fields
+		List<JTextField> textFields = Arrays.asList(tfId, tfLastName, tfFirstName, tfMi, tfAddress, tfCity, tfState,
+				tfTelephone);
+
+		// Implement view functionality
+		btnView.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Validation validation = new Validation(textFields);
+					
+					String Id = validation.getId(tfId.getText().trim());
+					if (Id.length() > 9) {
+						setPrompt(Id, true);
+						return;
+					} else {
+
+						PreparedStatement pstmtView = db.getPreparedViewStatement();
+						pstmtView.setString(1, Id);
+						ResultSet rs = pstmtView.executeQuery();
+						if (rs.next()) {
+							ResultSetMetaData rsmt = rs.getMetaData();
+							int columnCount = rsmt.getColumnCount();
+							for (int i = 1; i <= columnCount; i++) {
+								textFields.get(i - 1).setText(rs.getString(i));
+							}
+							setPrompt("Record found!");
+						} else {
+							setPrompt("No record found!", true);
+							clearTextFields();
+						}
+					}
+				}catch (ValidationException ve) {
+		            // If a validation error occurs, set the prompt and return
+		            setPrompt(ve.getMessage(), true);
+		            return;
+		        } catch (SQLException se) {
+		        	setPrompt("Database error occurred. Please try again later.", true);
+					se.printStackTrace();
+					return;
+				}
+			}
+
+		});
+
+		// Implement insert functionality
+		btnInsert.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Validation validation = new Validation(textFields);
+					validation.validate();
+					String Id = validation.getId(tfId.getText().trim());
+					if (Id.length() > 9) {
+						setPrompt(Id, true);
+						return;
+					} else {
+						PreparedStatement pstmtInsert = db.getPreparedInsertStatement();
+						if (db.idExists(Id)) {
+							setPrompt("This ID has been in the table, please try again", true);
+							return;
+						} else {
+							pstmtInsert.setString(1, Id);
+							for (int i = 1; i < textFields.size(); i++) {
+								pstmtInsert.setString(i + 1, textFields.get(i).getText().trim());
+							}
+							pstmtInsert.executeUpdate();
+							setPrompt("Insert a new data successfully!");
+						}
+					}
+				} catch (ValidationException ve) {
+		            // If a validation error occurs, set the prompt and return
+		            setPrompt(ve.getMessage(), true);
+		            return;
+				}catch (SQLException se) {
+					setPrompt("Database error occurred. Please try again later.", true);
+					se.printStackTrace();
+					return;
+				}
+			}
+		});
+
+		// Implement update functionality
+		btnUpdate.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Validation validation = new Validation(textFields);
+					validation.validate();
+					String Id = validation.getId(tfId.getText().trim());
+					if (Id.length() > 9) {
+						setPrompt(Id, true);
+						return;
+					} else {
+						PreparedStatement pstmtView = db.getPreparedViewStatement();
+						pstmtView.setString(1, Id);
+						ResultSet rs = pstmtView.executeQuery();
+						if (!rs.next()) {
+							setPrompt("This record is not included in the table, please try again", true);
+							return;
+						}
+
+						// Store the old data
+						String oldData = "Old Information:\n";
+						ResultSetMetaData rsmd = rs.getMetaData();
+						int columnCount = rsmd.getColumnCount();
+						for (int i = 1; i <= columnCount; i++) {
+							oldData += rsmd.getColumnName(i) + ": " + rs.getString(i) + "\n";
+						}
+
+						// Store the new data
+						String newData = "New Information:\n";
+						for (int i = 1; i < textFields.size(); i++) {
+							newData += rsmd.getColumnName(i + 1) + ": " + textFields.get(i).getText() + "\n";
+						}
+
+						// Confirm with the user
+						int dialogResult = JOptionPane.showConfirmDialog(null,
+								oldData + "\n" + newData + "\nDo you confirm the update?", "Confirm Update",
+								JOptionPane.YES_NO_OPTION);
+						if (dialogResult == JOptionPane.YES_OPTION) {
+							PreparedStatement pstmtUpdate = db.getPreparedUpdateStatement();
+							for (int i = 1; i < textFields.size(); i++) {
+								if (!textFields.get(i).getText().isEmpty()) {
+									pstmtUpdate.setString(i, textFields.get(i).getText());
+								} else {
+									pstmtUpdate.setString(i, rs.getString(i + 1));
+								}
+							}
+							pstmtUpdate.setString(textFields.size(), Id);
+							pstmtUpdate.executeUpdate();
+							setPrompt("Update a data successfully!");
+						} else {
+							setPrompt("Update cancelled by the user.");
+						}
+					}
+
+				}catch (ValidationException ve) {
+		            // If a validation error occurs, set the prompt and return
+		            setPrompt(ve.getMessage(), true);
+		            return;
+				} 
+				catch (SQLException se) {
+					setPrompt("Database error occurred. Please try again later.", true);
+					se.printStackTrace();
+					return;
+				}
+			}
+		});
+
+		// Implement clear functionality
+		btnClear.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clearTextFields();
+				setPrompt("Fields cleared!");
+			}
+		});
+
+		// Close connection when closing the window
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				try {
+					db.closeConnection();
+				} catch (SQLException | NullPointerException se) {
+					se.printStackTrace();
+				} finally {
+					System.exit(0);
+				}
+			}
+		});
+	}
+
+	// Helper methods
+	public void setPrompt(String msg) {
+		lblPrompt.setText(msg);
+		lblPrompt.setForeground(Color.BLACK);
+	}
+
+	public void setPrompt(String msg, boolean isError) {
+		lblPrompt.setText(msg);
+		lblPrompt.setForeground(Color.RED);
+	}
+
+	public void clearTextFields() {
+		tfId.setText("");
+		tfLastName.setText("");
+		tfFirstName.setText("");
+		tfMi.setText("");
+		tfCity.setText("");
+		tfState.setText("");
+		tfAddress.setText("");
+		tfTelephone.setText("");
+	}
+
+	// main method
+	public static void main(String[] args) {
+
+		App app = new App();
+
+		// Make the app visible
+		app.setVisible(true);
+
+	}
+
+}
---
+++ Validation.java
@@ -0,0 +1,105 @@
+package assignment4;
+
+import javax.swing.JTextField;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Validation {
+
+    private List<JTextField> textFields;
+    private String ID;
+    private static final int[] MAX_LENGTHS = {9, 15, 15, 1, 20, 20, 2, 10};
+    private static final int[] REQUIRED_FIELD_INDICES = {0, 1, 2, 7};
+    private static final String[] FIELDS= {"ID","Last name","First name","mi","Adress","City","State","Telephone"};
+
+    public Validation(List<JTextField> textFields) {
+        this.textFields = textFields;
+    }
+
+    public List<Integer> validateLength() {
+    	List<Integer> errorlist= new ArrayList<>();
+    	for (int i = 0; i < textFields.size(); i++) {
+            String fieldText = textFields.get(i).getText().trim();
+            if (fieldText.length() > MAX_LENGTHS[i]) {
+                errorlist.add(i);
+            }
+        }
+        return errorlist;
+    }
+
+    public List<Integer> validateRequiredFields() {
+    	List<Integer> errorlist= new ArrayList<>();
+        for (int index : REQUIRED_FIELD_INDICES) {
+            String fieldText = textFields.get(index).getText().trim();
+            if (fieldText.isEmpty()) {
+            	errorlist.add(index);
+            }
+        }
+        return errorlist;
+    }
+    
+    public boolean isEmpty(String str) {
+        return str == null || str.trim().isEmpty();
+    }
+
+    public boolean exceedsMaxLength(String str, int maxLength) {
+        return str.length() > maxLength;
+    }
+    
+    public boolean isIdNumeric(String id) {
+        
+        try {
+            Integer.parseInt(id);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public String getId(String IdRaw) throws ValidationException {
+        
+        String text=null;
+        
+        if (isEmpty(IdRaw)) {
+        	throw new ValidationException("ID can not be null.");
+        }else if (exceedsMaxLength(IdRaw, 9)) {
+        	throw new ValidationException("ID must be 9 characters or less, please try again");
+        }else if (!isIdNumeric(IdRaw)) {
+            throw new ValidationException("ID must be a number. Please try again");
+         }else{
+        String id = String.format("%09d", Integer.parseInt(IdRaw));
+        return id;}
+        }
+    
+    public class ValidationException extends Exception {
+        public ValidationException(String message) {
+            super(message);
+        }
+    }
+    
+    public void validate() throws ValidationException {
+        List<Integer> lenErrors = validateLength();
+        String text="";
+        String text1="";
+        if (!lenErrors.isEmpty()) {
+        	for(int i:lenErrors)
+        	{
+        		text+=FIELDS[i]+",";
+        		text1+=MAX_LENGTHS[i]+",";
+        	}
+            throw new ValidationException(text+"exceeds max length "+text1+". Please enter again.");
+        }
+        
+        List<Integer> requireErrors = validateRequiredFields();
+        String text2="";
+        
+        if (!requireErrors.isEmpty()) {
+        	for(int i:requireErrors) {
+        		text2+=FIELDS[i]+",";
+        	}
+            throw new ValidationException(text2+" can not be empty. Please try again.");
+        }
+    }
+
+}
